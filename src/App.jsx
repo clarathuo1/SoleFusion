7d7d513dbfa1be565b1934dfa242ad333abd655e
Clara Thuo <clarathuo0@gmail.com>
2024-08-15 22:44:03 +0300

    Finalizing and connecting

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import Footer from "./component/Footer"
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
 import DataDisplay from "./component/DataDisplay";
 //import RecipeComponent from "./component/DataDisplay";
 import HeroSection from "./component/HeroSection";
 import Viggan from "./component/Viggan";
+import Navbar from "./component/Navbar";
 // import Footer from "./component/MoreDetails";
 
 function App() {
 	return (
 		<div>
-  <Navbar />
+			<div>
+                <Navbar />
+
+			</div>
 			<div>
 				<HeroSection />
 			</div>
