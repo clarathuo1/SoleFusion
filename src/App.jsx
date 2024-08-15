92c7f85818345b481521dc759d44ccd83acccbb5
CAPTAIN-JOB <kiptoojob73@gmail.com>
2024-08-15 11:22:02 +0300

    deleted  someChanges

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 // import DataDisplay from "./component/DataDisplay";
 import RecipeComponent from "./component/DataDisplay";
+import HeroSection from "./component/HeroSection";
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<div>
-			<RecipeComponent /> 
-    <HeroSection />
+			{" "}
+			<div>
+				<HeroSection />
+			</div>
+			<div>
+				<RecipeComponent />
+			</div>
 		</div>
 	);
 }
 
+export default App;
