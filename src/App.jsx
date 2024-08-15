a2d0f708aa5f326f255577e1b3ac1e1eb97ed2e8
CAPTAIN-JOB <kiptoojob73@gmail.com>
2024-08-15 12:04:08 +0300

    i want tu  push  but  its  giving  me  some  errors  leme push  i see  what  will happen

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,14 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 // import DataDisplay from "./component/DataDisplay";
 import RecipeComponent from "./component/DataDisplay";
-<<<<<<< HEAD
 import HeroSection from "./component/HeroSection";
-=======
 import Viggan from "./component/Viggan";
 // import Footer from "./component/MoreDetails";
->>>>>>> ft-display-data
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-<<<<<<< HEAD
 		<div>
 			{" "}
 			<div>
@@ -24,17 +20,10 @@ function App() {
 			<div>
 				<RecipeComponent />
 			</div>
-		</div>
-=======
-		<main className=" bg-gray-100">
-			<div className=" m-3 shadow-sm">
-				<RecipeComponent />
-			</div>
 			<div className=" m-3 shadow-sm">
 				<Viggan />
 			</div>
-		</main>
->>>>>>> ft-display-data
+		</div>
 	);
 }
 
